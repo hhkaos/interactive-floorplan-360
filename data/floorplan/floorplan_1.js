cb996f4a6a3a400a3e400319476c40174a28c82c
Raúl Jiménez Ortega <hhkaos@gmail.com>
2021-04-17 18:48:57 +0200

    Adding floor plan interactivity

--- data/floorplan/floorplan_1.js
+++ data/floorplan/floorplan_1.js
@@ -1,6 +1,6 @@
 {
     "second-floor-hall-1": {
-        "panorama": "panoramas/second-floor-hall-1.jpg",
+        "panorama": "data/panoramas/second-floor-hall-1.jpg",
         "yaw": -78,
         "rotation": 12
     }
--- js/interactive-floorplan-360.js
+++ js/interactive-floorplan-360.js
@@ -6,9 +6,9 @@ const renderApp = function(config){
     let floorMenu = '';
     Object.keys(config.floorplans).forEach((elem, i) => {
         if(i===config.defaultFloor){
-            floorMenu += `<li class="active">${elem}</li>`
+            floorMenu += `<li class="active" data-floor="${elem}">${elem}</li>`
         }else{
-            floorMenu += `<li>${elem}</li>`
+            floorMenu += `<li data-floor="${elem}">${elem}</li>`
         }
 
     });
@@ -36,8 +36,72 @@ const renderApp = function(config){
     return new DOMParser().parseFromString(xmlString, "text/html").documentElement;
 };
 
-const renderFloorPlan = function(dom, config){
+const setPanorama = (floorPlanId) => {
+    document.querySelector("#panorama").innerHTML = ""
+    _IFP.panorama = pannellum.viewer('panorama', {
+        "default": {
+            "firstScene": _IFP.config.floorplans[floorPlanId].initialLocation
+        },
+        "scenes": _IFP.config.floorplans[floorPlanId].tourContent,
+        "autoLoad": true,
+        // "hotSpotDebug": true,
+    });
 
+    _IFP.panorama.on('scenechange', function (extWindow) {
+        const mySvg = _IFP.mySvg;
+        if (mySvg.querySelector('.active')) {
+            mySvg.querySelector('.active').classList.remove('active');
+            mySvg.querySelector('#' + extWindow).classList.add('active')
+        }
+    });
+};
+
+const setFloorPlan = (floorPlanId) => {
+    document.querySelector('#floorplan').data = _IFP.config.floorplans[floorPlanId].floorplan;
+};
+
+_IFP.setActiveFloor = (id) => {
+    _IFP.activeFloor = id;
+    const activePlan = document.querySelector('.floors .active');
+    if(activePlan){
+        document.querySelector('.floors .active').classList.remove('active');
+    }
+    document.querySelector(`.floors [data-floor="${id}"]`).classList.add('active');
+    setFloorPlan(id);
+    setPanorama(id);
+};
+
+_IFP.getActivePlan = () => _IFP.config.floorplans[_IFP.activeFloor];
+
+const getCurrentTranslate = function () {
+    const cx = _IFP.mySvg.querySelector('.active').getAttribute('cx');
+    const cy = _IFP.mySvg.querySelector('.active').getAttribute('cy');
+    return `translate(${cx} ${cy})`
+};
+
+const getInitialTranslate = function () {
+    const cx = _IFP.mySvg.querySelector('#'+_IFP.getActivePlan().initialLocation).getAttribute('cx');
+    const cy = _IFP.mySvg.querySelector('#'+_IFP.getActivePlan().initialLocation).getAttribute('cy');
+    return `translate(${cx} ${cy})`
+};
+
+const createTriangle = function () {
+    let polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+    polygon.id = "POV";
+    //https://www.triangle-calculator.com/?what=sss&a=50&b=50&c=50&submit=Solve
+    // Triangle vertex coordinates: [0; 0] [50; 0] [25; 43.301]
+    _IFP.mySvg.querySelector('#'+_IFP.getActivePlan().initialLocation)
+    const activePoint = _IFP.mySvg.querySelector('#'+_IFP.getActivePlan().initialLocation)
+    const x = parseFloat(activePoint.getAttribute('cx'))
+    const y = parseFloat(activePoint.getAttribute('cy'))
+    polygon.setAttribute("points", `0 0 50 0 25 43.301`);
+    polygon.setAttribute("style", "stroke:#660000; fill:#cc3333;");
+    polygon.setAttribute('transform', getInitialTranslate());
+    return polygon;
+};
+
+const renderFloorPlan = function(dom, config){
+    _IFP.config = config;
     const appEl = document.querySelector(dom);
     const promises = Array();
     if(appEl.innerHTML.trim() === ""){
@@ -46,8 +110,13 @@ const renderFloorPlan = function(dom, config){
         */
         appEl.appendChild(renderApp(config).querySelector("body > *"));
 
-        Object.keys(config.floorplans).forEach((elem, i) => {
+        document.querySelectorAll('.floors li').forEach(elem => {
+            elem.addEventListener('click', (evt) => {
+                _IFP.setActiveFloor(evt.target.innerText)
+            });
+        })
 
+        Object.keys(config.floorplans).forEach((elem, i) => {
             promises.push(
                 function(){
                     return fetch(config.floorplans[elem].tour)
@@ -55,7 +124,9 @@ const renderFloorPlan = function(dom, config){
                     )
                 });
             });
-            let promisesWorking = promises.map(fn => fn());
+
+            const promisesWorking = promises.map(fn => fn());
+
             Promise.all(promisesWorking)
             .then(response =>{
 
@@ -63,74 +134,23 @@ const renderFloorPlan = function(dom, config){
                     config.floorplans[i].tourContent = elem
                 });
 
-                // Init tour on floor 0
-                const initTour = config.floorplans[config.defaultFloor]
-                let tour = initTour.tourContent;
-                document.querySelector('#floorplan').data = initTour.floorplan;
+                _IFP.activeFloor = config.defaultFloor
 
-                const panorama = pannellum.viewer('panorama', {
-                    "default": {
-                        "firstScene": initTour.initialLocation,
-                    },
-                    "scenes": tour,
-                    "autoLoad": true,
-                    // "hotSpotDebug": true,
-                });
-
-                setTimeout(function () {
-                    const mouseoverEvent = new Event('mouseover');
-
-                    const firstEl = _IFP.mySvg.getElementById(config.floorplans[config.defaultFloor].initialLocation);
-                    firstEl.dispatchEvent(mouseoverEvent);
-                    firstEl.classList.add('active');
-                }, 300);
-
-                panorama.on('scenechange', function (extWindow) {
-                    const mySvg = _IFP.mySvg;
-                    if (mySvg.querySelector('.active')) {
-                        mySvg.querySelector('.active').classList.remove('active');
-                        mySvg.querySelector('#' + extWindow).classList.add('active')
-                    }
-                });
-
-                const getCurrentTranslate = function () {
-                    const cx = _IFP.mySvg.querySelector('.active').getAttribute('cx');
-                    const cy = _IFP.mySvg.querySelector('.active').getAttribute('cy');
-                    return `translate(${cx} ${cy})`
-                }
-
-                setInterval(() => {
-                    const mySvg = _IFP.mySvg;
-                    if (mySvg.querySelector('.active')) {
-                        const location = tour[mySvg.querySelector('.active').id];
-                        const currentYaw = panorama.getYaw();
-                        const initYaw = location.yaw;
-                        const newRotation = location.rotation - (initYaw - currentYaw);
-                        const newTransform = `${getCurrentTranslate()} rotate(${newRotation})`;
-                        mySvg.querySelector('#POV').setAttribute('transform', newTransform);
-                    }
-                }, 300);
-
-                const createTriangle = function () {
-                    let polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-                    polygon.id = "POV";
-                    //https://www.triangle-calculator.com/?what=sss&a=50&b=50&c=50&submit=Solve
-                    // Triangle vertex coordinates: [0; 0] [50; 0] [25; 43.301]
-                    polygon.setAttribute("points", "0 0 50 0 25 43.301");
-                    polygon.setAttribute("style", "stroke:#660000; fill:#cc3333;");
-                    return polygon;
-                };
+                _IFP.setActiveFloor(config.defaultFloor)
 
                 document.getElementById('floorplan').addEventListener('load', () => {
                     let mySvg;
+
                     _IFP.mySvg = mySvg = document.getElementById('floorplan').contentDocument;
                     svgPanZoom(mySvg.querySelector('svg'),{
                         zoomEnabled: true,
                         fit: 1,
                         center: 1
                     })
-                    // mySvg.querySelector('#Plano').appendChild(createTriangle());
+
                     mySvg.querySelector("svg > g").appendChild(createTriangle());
+
+                    const tour = _IFP.getActivePlan().tourContent;
 
                     Object.keys(tour).forEach((elem, i) => {
                         const el = mySvg.getElementById(elem);
@@ -145,7 +165,7 @@ const renderFloorPlan = function(dom, config){
                                 if (activeEl) {
                                     mySvg.querySelector('circle.active').classList.remove('active');
                                 }
-                                panorama.loadScene(evt.target.id);
+                                _IFP.panorama.loadScene(evt.target.id);
                             }
                         });
                         el.addEventListener('mouseenter', evt => {
@@ -153,12 +173,34 @@ const renderFloorPlan = function(dom, config){
                         });
                     });
 
+                    setTimeout(function () {
+                        const mouseoverEvent = new Event('mouseover');
+                        const firstEl = _IFP.mySvg.getElementById(_IFP.getActivePlan().initialLocation);
+                        firstEl.dispatchEvent(mouseoverEvent);
+                        firstEl.classList.add('active');
+                    }, 300);
+
+
+                    if(_IFP.interval){
+                        clearInterval(_IFP.interval);
+                    }
+
+                    _IFP.interval = setInterval(() => {
+                        const mySvg = _IFP.mySvg;
+
+                        if (mySvg.querySelector('.active')) {
+                            const location = tour[mySvg.querySelector('.active').id];
+                            const currentYaw = _IFP.panorama.getYaw();
+                            const initYaw = location.yaw;
+                            const newRotation = location.rotation - (initYaw - currentYaw);
+                            const newTransform = `${getCurrentTranslate()} rotate(${newRotation})`;
+                            mySvg.querySelector('#POV').setAttribute('transform', newTransform);
+                        }
+                    }, 300);
+
                 });
             })
         }
-
-
-
 
     };
 
